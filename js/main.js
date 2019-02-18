71d0858be613bb16ce5440c7ac1efcfba80ee2d5
Madu Carbonari <mariaeduarda.s.carbonari@gmail.com>
2019-02-18 14:46:28 -0300

    Primeiro commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function linkar(){
+	window.location.href = "prog.html#prog";
+}
+
+function linkar2(){
+	window.location.href = "design.html#design";
+}
+
+function linkar3(){
+	window.location.href = "digital.html#digital";
+}
+
+function linkar4(){
+	window.location.href = "mao.html#mao";
+}
